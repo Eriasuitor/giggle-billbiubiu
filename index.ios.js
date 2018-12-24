a8e2e48eb801cdb030ca3ce70d8eb3380366282b
English Lory <lory@EnglishdeMac.local>
2018-12-24 19:51:06 +0800

    daily

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,11 @@ import {
   StyleSheet,
   View,
   ScrollView,
-  PanResponder
+  PanResponder,
+  MaskedViewIOS,
+  Modal,
+  Image,
+  TouchableHighlight
 } from 'react-native';
 import Svg, {
   Circle,
@@ -25,7 +29,7 @@ import Svg, {
   Line,
   Rect,
   Use,
-  Image,
+  // Image,
   Symbol,
   Defs,
   LinearGradient,
@@ -37,7 +41,7 @@ import Svg, {
 } from 'react-native-svg';
 import { range } from 'lodash'
 
-export default class BillBiuBiu2 extends Component {
+export default class BillBiuBiu extends Component {
 
   static defaultProps = {
     radius: 50,
@@ -52,8 +56,54 @@ export default class BillBiuBiu2 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      position: {
-        charge: {
+      modalVisible: true,
+      wallet: {
+        charge2: {
+          startX: 0,
+          startY: 0,
+          yuan: {
+            value: 0,
+            addition: 0
+          },
+          cent: {
+            value: 0,
+            addition: 0
+          },
+          x: this.props.widthHalf,
+          y: this.props.heightHalf - this.props.radius,
+          trigger: false
+        },
+        charge3: {
+          startX: 0,
+          startY: 0,
+          yuan: {
+            value: 0,
+            addition: 0
+          },
+          cent: {
+            value: 0,
+            addition: 0
+          },
+          x: this.props.widthHalf,
+          y: this.props.heightHalf - this.props.radius,
+          trigger: false
+        },
+        charge4: {
+          startX: 0,
+          startY: 0,
+          yuan: {
+            value: 0,
+            addition: 0
+          },
+          cent: {
+            value: 0,
+            addition: 0
+          },
+          x: this.props.widthHalf,
+          y: this.props.heightHalf - this.props.radius,
+          trigger: false
+        },
+        charge5: {
           startX: 0,
           startY: 0,
           yuan: {
@@ -70,19 +120,24 @@ export default class BillBiuBiu2 extends Component {
         }
       }
     };
-    this.moveListener = PanResponder.create({
+  }
+
+  componentWillMount() {
+
+  }
+
+  addMoveListener(walletName) {
+    return PanResponder.create({
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
       onPanResponderGrant: (evt, gestureState) => {
 
-        this.state.position.charge.trigger = true
+        this.state.wallet[walletName].trigger = true
 
         let angle = Math.atan2(evt.nativeEvent.locationY - this.props.heightHalf, evt.nativeEvent.locationX - this.props.widthHalf)
         angle += Math.PI / 2
         angle < 0 ? angle += Math.PI * 2 : undefined
-        this.state.position.charge.angle = angle
-
-
+        this.state.wallet[walletName].angle = angle
       },
       onPanResponderMove: (evt, gestureState) => {
 
@@ -90,78 +145,82 @@ export default class BillBiuBiu2 extends Component {
         angle += Math.PI / 2
         angle < 0 ? angle += Math.PI * 2 : undefined
 
-        if (this.state.position.charge.trigger) {
-          this.state.position.charge.trigger = false
-          if (this.state.position.charge.angle < angle) this.state.position.charge.operate = this.state.position.charge.yuan
-          else this.state.position.charge.operate = this.state.position.charge.cent
+        if (this.state.wallet[walletName].trigger) {
+          this.state.wallet[walletName].trigger = false
+          if (this.state.wallet[walletName].angle < angle) this.state.wallet[walletName].operate = this.state.wallet[walletName].yuan
+          else this.state.wallet[walletName].operate = this.state.wallet[walletName].cent
         }
-        if (this.state.position.charge.operate === this.state.position.charge.yuan)
-          if (this.state.position.charge.angle > Math.PI * 7 / 8 && angle < Math.PI / 8) this.state.position.charge.operate.addition++
-          else if (this.state.position.charge.angle < Math.PI / 8 && angle > Math.PI * 7 / 8) this.state.position.charge.operate.addition--
+        if (this.state.wallet[walletName].operate === this.state.wallet[walletName].yuan)
+          if (this.state.wallet[walletName].angle > Math.PI * 7 / 8 && angle < Math.PI / 8) this.state.wallet[walletName].operate.addition++
+          else if (this.state.wallet[walletName].angle < Math.PI / 8 && angle > Math.PI * 7 / 8) this.state.wallet[walletName].operate.addition--
 
-        this.state.position.charge.angle = angle  
+        this.state.wallet[walletName].angle = angle
 
-        this.state.position.charge.operate.value = Math.floor(angle / Math.PI / 2 * this.props.scale) + this.state.position.charge.operate.addition * this.props.scale
-        this.state.position.charge.x = Math.sin(angle) * this.props.radius + this.props.widthHalf
-        this.state.position.charge.y = this.props.heightHalf - Math.cos(angle) * this.props.radius
+        this.state.wallet[walletName].operate.value = Math.floor(angle / Math.PI / 2 * this.props.scale) + this.state.wallet[walletName].operate.addition * this.props.scale
+        this.state.wallet[walletName].x = Math.sin(angle) * this.props.radius + this.props.widthHalf
+        this.state.wallet[walletName].y = this.props.heightHalf - Math.cos(angle) * this.props.radius
         this.setState(this.state)
+
       }
     })
   }
 
-  componentWillMount() {
-
-  }
-
-
-
   render() {
     return (
-      <View style={{ padding: 100 }}>
-        <Svg width={this.props.width} height={this.props.height}>
-          <Text>{this.state.position.charge.yuan.value} -- {this.state.position.charge.cent.value} -- {this.state.position.charge.angle}</Text>
-          <Circle cx={this.props.widthHalf} cy={this.props.heightHalf} r={this.props.radius} fill="none" stroke="pink" strokeWidth="10" strokeOpacity="0.3" />
-          <Circle cx={this.state.position.charge.x} cy={this.state.position.charge.y} r={this.props.touchRadius} fill="pink" {...this.moveListener.panHandlers} />
-        </Svg>
-      </View>
-      /**   
-      <View
-        style={styles.container}
-        onStartShouldSetResponder={(evt) => true}
-        onMoveShouldSetResponder={(evt) => true}
-        onResponderMove={evt => {
-          let x = evt.nativeEvent.locationX - 50,
-            y = evt.nativeEvent.locationY - 50,
-            sc = 45 / Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5),
-            na = Math.atan(y / x)
-
-          this.state.value2 = na
-
-          if (x > 0 && y > 0) this.state.value1 = na / 2 / Math.PI * 100
-          if (x < 0 && y > 0) this.state.value1 = (Math.PI + na) / 2 / Math.PI * 100
-          if (x < 0 && y < 0) this.state.value1 = (Math.PI + na) / 2 / Math.PI * 100
-          if (x > 0 && y < 0) this.state.value1 = (na) / 2 / Math.PI * 100
-
-          this.state.value1 = Math.floor(this.state.value1) + 25
-          this.state.a = x * sc + 50
-          this.state.b = y * sc + 50
-          if (this.state.a < 50) this.state.big = 1
-          else this.state.big = 0
-          this.setState(this.state)
-        }}
-      >
-        <Svg
-          height="105"
-          width="105"
+      <View style={{ marginTop: 40, flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+        {
+          Object.keys(this.state.wallet).map(_w => {
+            return (
+              <Svg key={_w} width={this.props.width} height={this.props.height}>
+                <Circle cx={this.props.widthHalf} cy={this.props.heightHalf} r={this.props.radius} fill="none" stroke="pink" strokeWidth="10" strokeOpacity="0.3" />
+                <Circle cx={this.state.wallet[_w].x} cy={this.state.wallet[_w].y} r={this.props.touchRadius} fill="pink" {...this.addMoveListener(_w).panHandlers} />
+                <G x={this.props.widthHalf} y={this.props.heightHalf} onPress={() => alert(1)}>
+                  <Circle r={this.props.radius - 10} opacity='0'></Circle>
+                  <Text textAnchor='middle' y='-30' fontSize='24'>{this.state.wallet[_w].yuan.value}.{this.state.wallet[_w].cent.value}</Text>
+                  <Text textAnchor='middle' fontSize='10'>Balance</Text>
+                  <Text textAnchor='middle' y='10'>{this.state.wallet[_w].yuan.value || 1023}.{this.state.wallet[_w].cent.value || 49}</Text>
+                </G>
+              </Svg>
+            )
+          })
+        }
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert("Modal has been closed.");
+          }}
         >
-          <Text>{this.state.value1}</Text>
-          <Text y="20">{this.state.value2}</Text>
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Text>Hello World!</Text>
 
-          <Circle cx="50" cy="50" r="45" fill="none" stroke="pink" strokeWidth="10" strokeOpacity="0.3" />
-          <Path d={"M50 5 A45 45 0 " + this.state.big + " 1 " + this.state.a + " " + this.state.b} fill="none" stroke="pink" strokeWidth="9" strokeLinecap="round" />
-          <Circle cx={this.state.a} cy={this.state.b} r="7" fill="pink" onPress={(s) => alert('a?' + Object.keys(s))} />
-        </Svg>
-      </View>**/
+              <TouchableHighlight
+                onPress={() => {
+                  this.state.modalVisible = !this.state.modalVisible
+                  this.setState(this.state)
+                }}
+              >
+              <Svg height='100' width='100'>
+
+                <Text>Hide Modal</Text>
+
+              </Svg>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.state.modalVisible = !this.state.modalVisible
+            this.setState(this.state)
+          }}
+        >
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
@@ -172,6 +231,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  button: {
+    margin: 10,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
   welcome: {
     fontSize: 20,
@@ -185,4 +250,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('BillBiuBiu2', () => BillBiuBiu2);
+AppRegistry.registerComponent('BillBiuBiu', () => BillBiuBiu);
